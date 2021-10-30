f6a499a9b6e4bd7aeb4cad5c557a247a17b26f32
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:52:56 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-WordToVec',
-    version='1.0.4',
+    version='1.0.5',
     packages=['WordToVec'],
     url='https://github.com/StarlangSoftware/WordToVec-Py',
     license='',
     author='olcay',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Word2Vec Library',
-    install_requires=['NlpToolkit-Corpus']
+    install_requires=['NlpToolkit-Corpus'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
