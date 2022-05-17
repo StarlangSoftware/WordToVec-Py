42c70f090cefbaeafa21e5a4d4ac3a81ce71bcf1
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 23:16:33 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-WordToVec',
-    version='1.0.5',
+    version='1.0.6',
     packages=['WordToVec'],
     url='https://github.com/StarlangSoftware/WordToVec-Py',
     license='',
